423d3a846a9e0b492603ac315626360608f7db63
韩旭gogogo <js_bin@163.com>
2026-04-24 15:14:19 +0800

    chore: links update

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -6,7 +6,10 @@ const Team = () => {
       image: "/images/avator1.png",
       name: "Ferris",
       position: "CEO, Founder",
-      socialLinks: socialLinks,
+      socialLinks: [
+        { icon: "fa-facebook", href: "https://www.facebook.com/share/1Ds7AwLaNN/" },
+        ...socialLinks,
+      ],
     },
     {
       image: "/images/avator3.png",
@@ -27,6 +30,7 @@ const Team = () => {
       name: "Cassie",
       position: "Marketing Manager",
       socialLinks: [
+        { icon: "fa-facebook", href: "https://www.facebook.com/share/1KY6spaFtr/" },
         { icon: "fa-facebook", href: "https://www.facebook.com/share/1GdhtaFmfF/?mibextid=wwXIfr" },
         { icon: "fa-linkedin", href: "https://www.linkedin.com/in/cassie-huang-791537323/" },
         { icon: "fa-whatsapp", href: "https://wa.me/8613859190905" },
--- src/config/social.js
+++ src/config/social.js
@@ -1,5 +1,5 @@
 export const socialConfig = {
-  facebook: "https://www.facebook.com/profile.php?id=61577505150556",
+  facebook: "https://www.facebook.com/share/1HX98Ym3K1/",
   twitter: "https://x.com/ferrislee911",
   linkedin: "https://www.linkedin.com/company/%E5%A4%9A%E5%A3%AB%E9%81%94%E5%9C%8B%E9%9A%9B%E5%95%86%E5%8B%99%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8/?viewAsMember=true",
   youtube: "https://www.youtube.com/@ferris-gogogo",
